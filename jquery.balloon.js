65aa1ed91c92d20b00c49674042ea30ff1f4aa62
urin <urinkun@gmail.com>
2013-01-27 23:30:38 +0900

    Increment ver.

--- jquery.balloon.js
+++ jquery.balloon.js
@@ -6,7 +6,7 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  * @author: Hayato Takenaka (http://urin.github.com)
- * @version: 0.3.3 - 2012/04/18
+ * @version: 0.3.4 - 2012/04/18
 **/
 ;(function($) {
 	//-----------------------------------------------------------------------------
