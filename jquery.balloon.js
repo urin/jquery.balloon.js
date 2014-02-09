16245959ce62a96d3dd315d5efb9d17ae604eb84
urin <urinkun@gmail.com>
2014-02-09 16:44:47 +0530

    To update registry of jquery official plugin code base.

--- jquery.balloon.js
+++ jquery.balloon.js
@@ -6,7 +6,7 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  * @author: Hayato Takenaka (http://urin.github.com)
- * @version: 0.4.1 - 2014/02/09
+ * @version: 0.4.2 - 2014/02/09
 **/
 (function($) {
   //-----------------------------------------------------------------------------
